5daf71afd9dfe5cc9944030eb373ba100a094f9a
Henry-ai443 <henrymaina2024@outlook.com>
2025-10-20 05:35:42 -0400

    Added login endpoint

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer
+from django.contrib.auth import authenticate
 import logging
 
 logger = logging.getLogger(__name__)
@@ -27,3 +28,28 @@ class RegisterView(APIView):
                 logger.error("Error during registration", exc_info=True)
                 return Response({"error": "Internal server error during registration."}, status=500)
         return Response(serializer.errors, status=400)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if not email or not password:
+            return Response({
+                "message": "Both email and password are required"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(request, email=email, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "message": "Login successful",
+                "user_details": {
+                    "email": user.email,
+                },
+                "token": token.key
+            }, status=status.HTTP_200_OK)
+
+        return Response({
+            "message": "Invalid email or password"
+        }, status=status.HTTP_400_BAD_REQUEST)
